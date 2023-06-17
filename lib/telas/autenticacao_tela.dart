530f3136bd7e1dd766b863ed2eea273810fe2959
Amarante Silva <amaranthwfs@gmail.com>
2023-06-17 10:25:30 -0300

    criacao do formulario

--- lib/telas/autenticacao_tela.dart
+++ lib/telas/autenticacao_tela.dart
@@ -1,9 +1,15 @@
 import 'package:exercicio/_comum/minhas_cores.dart';
 import 'package:flutter/material.dart';
 
-class AutenticacaoTela extends StatelessWidget {
+class AutenticacaoTela extends StatefulWidget {
   const AutenticacaoTela({super.key});
 
+  @override
+  State<AutenticacaoTela> createState() => _AutenticacaoTelaState();
+}
+
+class _AutenticacaoTelaState extends State<AutenticacaoTela> {
+  bool queroEntrar = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,20 +28,83 @@ class AutenticacaoTela extends StatelessWidget {
               ),
             ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset("assets/logo.png", height: 128),
-              const Text(
-                "GymApp",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontSize: 48,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Image.asset("assets/logo.png", height: 128),
+                      const Text(
+                        "GymApp",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 48,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          label: Text("E-mail"),
+                        ),
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          label: Text(
+                            "Senha",
+                          ),
+                        ),
+                        obscureText: true,
+                      ),
+                      Visibility(
+                        visible: !queroEntrar,
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                label: Text(
+                                  "Confirme Senha",
+                                ),
+                              ),
+                              obscureText: true,
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                label: Text("Nome"),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(
+                        height: 32,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text((queroEntrar) ? "Entrar" : "Cadastre-se"),
+                      ),
+                      const Divider(),
+                      TextButton(
+                        onPressed: () {
+                          setState(() {
+                            queroEntrar = !queroEntrar;
+                          });
+                        },
+                        child: Text((queroEntrar)
+                            ? "Ainda não tem uma conta? cadastre-se!"
+                            : "Já tem uma conta? Entre!"),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-            ],
+            ),
           ),
         ],
       ),
