f450c1cf8dc2d2cd7030ee862ac200f3477a36e7
Amarante Silva <amaranthwfs@gmail.com>
2023-06-17 08:35:01 -0300

    first commit

---
+++ lib/telas/exercicio_tela.dart
@@ -0,0 +1,146 @@
+import 'package:exercicio/modelos/exercicio_modelo.dart';
+import 'package:exercicio/modelos/sentimento_modelo.dart';
+import 'package:flutter/material.dart';
+
+class ExercicioTela extends StatelessWidget {
+  ExercicioTela({super.key});
+
+  final ExercicioModelo exercicioModelo = ExercicioModelo(
+    id: "001",
+    nome: "Remada baixa supinada",
+    treino: "Treino A",
+    comoFazer: "Segura a barra e puxa",
+    urlImagem: "",
+  );
+
+  final List<SentimentoModelo> listaSentimentos = [
+    SentimentoModelo(
+      id: "SE001",
+      sentindo: "Sentindo pouca ativacao",
+      data: "2023/06/13",
+    ),
+    SentimentoModelo(
+      id: "SE002",
+      sentindo: "já Senti alguma ativacao",
+      data: "2023/06/15",
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Column(
+            children: [
+              Text(
+                exercicioModelo.nome,
+                style:
+                    const TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+              ),
+              Text(
+                exercicioModelo.treino,
+                style: const TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+          centerTitle: true,
+          backgroundColor: const Color(0xFF0A6D92),
+          elevation: 0,
+          toolbarHeight: 72,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(
+              bottom: Radius.circular(32),
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print('FAB foi clicado');
+          },
+          child: const Icon(Icons.add),
+        ),
+        body: Container(
+          margin: const EdgeInsets.all(8),
+          padding: const EdgeInsets.all(16.0),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: ListView(
+            children: [
+              SizedBox(
+                height: 250,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        print('EB foi clicado');
+                      },
+                      child: const Text('Enviar foto'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Tirar foto"),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              const Text(
+                'Como fazer?',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Text(exercicioModelo.comoFazer),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Divider(
+                  color: Colors.black,
+                ),
+              ),
+              const Text(
+                'Como estou me sentido?',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: List.generate(listaSentimentos.length, (index) {
+                  SentimentoModelo sentimentoAgora = listaSentimentos[index];
+                  return ListTile(
+                    dense: true,
+                    contentPadding: EdgeInsets.zero,
+                    title: Text(sentimentoAgora.sentindo),
+                    subtitle: Text(sentimentoAgora.data),
+                    leading: const Icon(Icons.double_arrow),
+                    trailing: IconButton(
+                      onPressed: () {
+                        print("Deletar ${sentimentoAgora.sentindo}");
+                      },
+                      icon: const Icon(
+                        Icons.delete,
+                        color: Colors.red,
+                      ),
+                    ),
+                  );
+                }),
+              )
+            ],
+          ),
+        ));
+  }
+}
